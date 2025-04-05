486a443ff92c729aa1b04def4bce077f94d88b5f
GhostOcelot <houseplayer@gmail.com>
2025-04-05 15:18:39 +0200

    improve repository list

--- src/GithubSearch/RepositoryList.tsx
+++ src/GithubSearch/RepositoryList.tsx
@@ -7,16 +7,19 @@ interface Props {
 const RepositoryList = ({ data }: Props) => {
   return (
     <ul className="w-2/3 max-w-[600px] m-8">
+      <li
+        key="header"
+        className="font-semibold border-b-1 border-b-amber-200 py-2 pl-2 grid grid-cols-[70px_1fr]"
+      >
+        <p>stars</p>
+        <p>name</p>
+      </li>
       {data &&
         data.items.map((item) => (
-          <li key={item.id} className="border-b-1 border-b-amber-200 py-1">
-            <a
-              href={item.html_url}
-              target="blank"
-              className="grid grid-cols-[70px_1fr] hover:text-amber-600"
-            >
-              <p className="inline-block pr-8"> {item.stargazers_count} </p>
-              <p className="font-bold"> {item.name} </p>
+          <li key={item.id} className="border-b-1 border-b-amber-200 py-2 pl-2 hover:bg-gray-100">
+            <a href={item.html_url} target="blank" className="grid grid-cols-[70px_1fr]">
+              <p> {item.stargazers_count} </p>
+              <p> {item.name} </p>
             </a>
           </li>
         ))}
